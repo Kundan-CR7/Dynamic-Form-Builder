f761c4e7247185c7549c37d647cc63b51a94895c
Kundan-CR7 <Kundangupta7334@gmail.com>
2025-10-04 13:24:28 +0530

    basic folder structure setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,() => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-3xl'>
+      Welcome to Dynamic Form Builder App
+    </div>
+    </>
+  )
+}
+
+export default App
